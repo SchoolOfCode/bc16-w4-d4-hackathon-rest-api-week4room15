86cf506f3b69032fdb1e0eed6b4827389b2b307c
JakFoster <jakfoster97@outlook.com>
2024-03-01 10:49:45 +0000

    completed post request handler and helper function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { v4 as uuidv4 } from "uuid";
+import { getTodoList, addTodo } from "./models/todos.js";
 
 const app = express();
 app.use(express.json());
@@ -15,14 +16,36 @@ app.listen (port, () => {
 app.get("/todos", async function (req, res) {
   // Try catch block containing the below...
   try {
-    // create variable that = the reseult of the async function (must await)
+    // create variable that = the result of the async function (must await)
     const todos = await getTodoList();
     // parse the result into a json object and send status code - 200
     res.status(200).json({ success: true, payload: todos });
   } catch (error) {
+    // in catch block - parse the result into a json object and send status code - server error code 500
     res.status(500).json({ success: false, payload: `${error} unexpected error` });
   }
 });
 
+// create post request to endpoint /todos
+// pass second argument as an async function
+app.post("/todos", async function (req, res) {
+  // Try catch block containing the below...
+  try {
+    //Get the request body
+    const { toDo, completed } = req.body;
+    const newTodo = {id: uuidv4(), toDo, completed}
+    // create variable that = the result of the async function (must await)
+    const todo = await addTodo(newTodo);
+    //If client input is incorrect, throw error
+    if(!toDo || toDo.trim() === "" || !completed || completed.trim() === ""){
+      throw new Error('Todo List properties incorrect');
+    }
+    // parse the result into a json object and send status code - 201
+    res.status(201).json({ success: true, payload: todo });
+  } catch (error) {
+    //Dynamically return correct error status and message
+    const statusCode = error.statusCode || 500;
+    res.status(statusCode).json({ success: false, payload : error.message || 'Internal server error' });
+  }
+});
 
-// in catch block - parse the result into a json object and send status code - server error code 500
--- models/todos.js
+++ models/todos.js
@@ -1,3 +1,13 @@
 import data from "../data/todos.json" assert {type : "json"};
 
-console.log(data);
+//Create helper function which returns the whole array
+export async function getTodoList() {
+    return data;
+}
+
+//Create helper function which returns the Todo
+export async function addTodo(todo) {
+    const todos = await getTodoList();
+    todos.push(todo);
+    return todo;
+} 
